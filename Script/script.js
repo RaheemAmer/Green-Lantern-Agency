a83e8c4d014d7955cd36c9d7a6d4d2d67d53a241
raheemamer <raheem.amer22@gmail.com>
2022-06-06 03:44:48 +0200

    Project Done

--- Script/script.js
+++ Script/script.js
@@ -15,3 +15,22 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+var myButton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
